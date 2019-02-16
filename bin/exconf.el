9163e82bdfc535afabddbbbf23a6eb9e250f2cf5
Philip K <philip@warpmail.net>
2019-02-16 12:52:40 +0100

    Fixed indentation issues with emacs.d.html

--- bin/exconf.el
+++ bin/exconf.el
@@ -1,27 +1,40 @@
 ;; to be executed with `emacs -nw -Q --script exconf.el'
 
-(require 'org)
+(add-to-list 'load-path (expand-file-name (car (file-expand-wildcards "~/.emacs.d/elpa/htmlize-*"))))
 
-(add-to-list 'load-path (expand-file-name "elpa/htmlize-1.53" user-emacs-directory))
+(require 'org)
 (require 'htmlize)
 
-(setq-default make-backup-files nil
-			  org-html-doctype "xhtml5"
-			  org-html-link-home "/"
-			  org-html-head-include-scripts nil
-			  org-html-html5-fancy t
-			  org-html-head "<link rel=\"stylesheet\" href=\"/normalize.css\" />
+(setq make-backup-files nil
+	  org-edit-src-content-indentation 0
+	  org-html-doctype "xhtml5"
+	  org-html-link-home "/"
+	  org-html-head-include-scripts t
+	  org-html-html5-fancy t
+	  org-html-head "<link rel=\"stylesheet\" href=\"/normalize.css\" />
 <style>
-body { margin: 0.5em; }
-div#content {
-	max-width: 60em;
+body {
+	margin: 1em auto 0 auto;
+    display: table;
+    line-height: 1.2em;
 	text-align: justify;
-	margin: 2% auto;
-    font-family: Tahoma, Geneva, sans-serif;
+    font-family: \"Open Sans\", \"Noto Sans\", sans-serif;
+    color: #222;
 }
+code, kbd, tt, pre {
+	font-family: Consolas, Hack, Monaco, Insonsolata, monospace, monospace;
+}
+
+#content { 	max-width: 60em; }
 </style>")
 
 (find-file (expand-file-name "conf.org" user-emacs-directory))
+
+;;; replace all tabs with 4 spaces
+(goto-char (point-min))
+(while (search-forward "\t" nil t)
+    (replace-match "    "))
+
 (org-html-export-as-html)
 (switch-to-buffer "*Org HTML Export*")
 (write-file (expand-file-name "~/code/web/www/www/emacs.d.html"))
